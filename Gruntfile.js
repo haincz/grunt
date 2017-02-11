0a24fa6c610fb3cbe5d6165020ffe61399363830
Mateusz Mikoś <Mateusz Miko>
2017-02-11 21:06:27 +0100

    task runner correction

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
           'style.css': 'style.sass'
         }
       }
-    }
+    },
 	
 	imagemin: {
   		dynamic: {
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
   				dest: 'images/build/'
   			}]
   		}
-  	}
+  	},
 	
 	watch: {
     scripts: {
